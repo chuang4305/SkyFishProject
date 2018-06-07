7493c7ec7f7458f741cf68fce2387e3d462bef03
T-Gould <tgould@mail.bradley.edu>
2018-06-07 13:48:05 -0500

    Part of combat system.
    Check of input strings. make key input within a certain time. Start of using mobile controls.

---
+++ SkyFishProject/Assets/Script/Tyler Scripts/EnableCombo.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnableCombo : MonoBehaviour
+{
+
+    // author: Kamil104
+    // http://answers.unity3d.com/users/225838/kamil1064.html
+
+    public GameObject comboPanel;
+    public KeyCode startCombo;
+    public float _timer;
+    public int _comboLength;
+
+
+    public Button m_YourButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = m_YourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        //if (Input.GetKeyDown(startCombo) && comboPanel.activeSelf == false) // if you press key and combo game isn't already started
+        //{
+            comboPanel.SetActive(true);
+            // you may use Random.range() here for borh variables
+
+            comboPanel.GetComponent<fight_combo>().SetUpGame(_timer, _comboLength); // sending timer and cobmo length data
+        //}
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ SkyFishProject/Assets/Script/Tyler Scripts/fight_combo.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fight_combo : MonoBehaviour
+{
+
+    // author: Kamil104
+    // http://answers.unity3d.com/users/225838/kamil1064.html
+    public Image[] images; // imgaes to show which buton you need to press
+    public KeyCode[] keys; // keys you need to press
+    private float timer; // time which you have to press
+    private float sorryTimer = 0;
+    private int current;
+    private int comboLength; // how many times you need to press correct key
+    private int currentCombo = 0;
+    public int damage;
+    public Text damageText;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        damageText.text = "Damage = " + damage;
+
+        if (Time.time > sorryTimer)
+        {
+            SetTimer(false);
+        }
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(keys[current]))
+            {
+              
+                SetTimer(true);
+            }
+            else
+            {
+                damage = damage - 2;
+                print("wrong key was pressed: reduced damage = 2;");
+                //gameObject.SetActive(false);
+            }
+        }
+
+        images[current].fillAmount = (sorryTimer - Time.time) / timer;
+    }
+    public void SetTimer(bool fastEnough)
+    {
+        if (currentCombo >= comboLength)
+        {
+            images[current].fillAmount = 0;
+           // print("you won :)");
+            print(damage);
+            // do some stuff here
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            sorryTimer = Time.time + timer;
+            images[current].fillAmount = 0;
+            current = Random.Range(0, images.Length);
+            if (!fastEnough)
+            {
+                print("failed to press key: reduced damage = 2");
+                damage = damage - 2;
+                // and do die stuff here
+               
+            }
+            else
+                print("Nice");
+            currentCombo++;
+        }
+    }
+    public void SetUpGame(float my_timer, int my_comboLength)
+    {
+        timer = my_timer;
+        comboLength = my_comboLength;
+        sorryTimer = Time.time + timer;
+        currentCombo = 1;
+    }
+}
